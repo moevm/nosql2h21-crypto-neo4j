1d968832323e2eab28d2da5b4b53040f1337b251
gagask <kholkovskii@mail.ru>
2021-12-23 19:03:42 +0300

    add class DbMS

--- main.py
+++ main.py
@@ -1,37 +1,17 @@
-from flask import Flask
-from neo4j import GraphDatabase
-
-
-class HelloWorldExample:
-    def __init__(self, uri, user, password):
-        self.driver = GraphDatabase.driver(uri, auth=(user, password))
-
-    def close(self):
-        self.driver.close()
-
-    def greeting(self, message):
-        with self.driver.session() as session:
-            greeting = session.write_transaction(self._create_and_return_greeting, message)
-            return greeting
-
-    @staticmethod
-    def _create_and_return_greeting(tx, message):
-        result = tx.run("CREATE (a:Greeting) "
-                        "SET a.message = $message "
-                        "RETURN a.message + ', from node ' + id(a)", message=message)
-        return result.single()[0]
+from flask import Flask, render_template, request
+from DbMS import DbMS
 
 
 app = Flask(__name__)
+db = DbMS("bolt://localhost:7687", "neo4j", "password")
 
 
 @app.route('/')
 def hello_world():
-    greeter = HelloWorldExample("bolt://localhost:7687", "neo4j", "password")
-    res = greeter.greeting("hello, world")
-    greeter.close()
+    res = db.greeting("hello, world")
     return res
+    # return render_template(*.html)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
