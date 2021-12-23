722fe88628ff8a74b1a0bd61edc5ac2d3c69d60d
gagask <kholkovskii@mail.ru>
2021-12-23 19:19:17 +0300

    add reroute

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ from DbMS import DbMS
 
 
 app = Flask(__name__)
-db = DbMS("bolt://localhost:7687", "neo4j", "password")
+# db = DbMS("bolt://localhost:7687", "neo4j", "password")
 
 
-@app.route('/')
-def hello_world():
-    res = db.greeting("hello, world")
-    return res
-    # return render_template(*.html)
+@app.route('/balance')
+def balance():
+    # res = db.greeting("hello, world")
+    # return res
+    return render_template("balance.html")
+
+
+@app.route('/history')
+def history():
+    # res = db.greeting("hello, world")
+    # return res
+    return render_template("History.html")
 
 
 if __name__ == '__main__':
